c58c63806c6936cfcff528f6e4e0b3458bd4ff14
John Jenson <JRRJ@users.noreply.github.com>
2018-02-27 00:25:06 -0800

    generate app structure with angular cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ssv-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ssv';
+}
